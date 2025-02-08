49afa0b69527c54ad2b2fbb227685b9b66cd2d82
Lukasz Gendek <lukasz.gendek@gmail.com>
2025-02-08 09:48:14 +0100

    test skeleton for the first scenario

--- src/test/java/com/sportradar/scoreboard/ScoreboardStepDefinitions.java
+++ src/test/java/com/sportradar/scoreboard/ScoreboardStepDefinitions.java
@@ -1,5 +1,24 @@
 package com.sportradar.scoreboard;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class ScoreboardStepDefinitions {
+    @Given("an empty scoreboard")
+    public void an_empty_scoreboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I start a new match with home team {string} and away team {string}")
+    public void i_start_a_new_match_with_home_team_and_away_team(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the scoreboard should contain one match with the score {string}")
+    public void the_scoreboard_should_contain_one_match_with_the_score(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }
